c6e4299bad3106ed3e64a184e0968ef214da5f97
orest <oresttrue@gmail.com>
2020-08-12 20:36:09 +0300

    added app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-let basket = document.getElementById("clickme"),
-  count = 0;
-basket.onclick = function() {
-  count += 1;
-  basket.innerHTML = " " + count;
-};
+let productsCountEl = document.getElementById("products-count");
+let addToCartButtons = document.querySelectorAll(".button2");
+
+// console.log(addToCartButtons);
+
+for(let i = 0; i < addToCartButtons.length; i++) {
+  addToCartButtons[i].addEventListener("click",function() {
+    productsCountEl.textContent = +productsCountEl.textContent + 1;
+  })
+}
