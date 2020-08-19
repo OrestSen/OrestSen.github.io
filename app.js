314acae5adfd3cd369aa42a60b7cf78086654906
orest <oresttrue@gmail.com>
2020-08-19 09:04:30 +0300

    added slick end change style

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
+// add to cart
+
 let productsCountEl = document.getElementById("products-count");
 let addToCartButtons = document.querySelectorAll(".button2");
 
 // console.log(addToCartButtons);
+  // multiple
+  for(let i = 0; i < addToCartButtons.length; i++) {
+    addToCartButtons[i].addEventListener("click",function() {
+      productsCountEl.textContent = +productsCountEl.textContent + 1;
+    })
+  }
+// single
+// addToCartButtons.addEventListener("click", function(e) {
+//   let current = e.target;
+//   console.log(current)
+//   current.style.backgroundColor = "red"
+// })
 
-for(let i = 0; i < addToCartButtons.length; i++) {
-  addToCartButtons[i].addEventListener("click",function() {
-    productsCountEl.textContent = +productsCountEl.textContent + 1;
+// change like button state
+
+
+let likeButtons = document.querySelectorAll(".like");
+
+
+for(let i = 0; i < likeButtons.length; i++) {
+  likeButtons[i].addEventListener("click",function() {
+    this.classList.toggle("liked")
   })
 }
+
+// slider slick
+
+$(".slider-block").slick({
+  dots:true,
+})
